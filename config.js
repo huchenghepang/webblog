4729db998b8efd2ac3c4c654e7c803b3b3a015a9
Jeff <2927678784@qq.com>
2024-12-18 18:20:35 +0800

    修改Mysql初始默认密码

--- config.js
+++ config.js
@@ -6,21 +6,21 @@ module.exports = {
     expiresIn: "1d", // token的保留时间,  '1h' '2d' ‘1d 设置为数字则为毫秒 
     serverPort: 5797,
     webSiteName: "Sky of City",
-    hostname: "175.178.195.144",
+    hostname: "huchenghe.site", // 服务器的域名或地址
     urlADDRESS: process.env.NODE_ENV === 'production' ? "https://huchenghe.site" : "http://localhost:5797", // 根据环境调整URL
-    REDIS_HOST: process.env.NODE_ENV === 'production' ? "127.0.0.1" : "175.178.195.144",
+    REDIS_HOST: process.env.NODE_ENV === 'production' ? "127.0.0.1" : "127.0.0.1", // 连接的服务器地址
     REDIS_PORT: 6379,
-    REDIS_PASSWORD: 15970460916,// 远程连接为了安全性需要有
+    REDIS_PASSWORD: 123456,// 远程连接为了安全性需要有  默认为123456 改动这里需要同时改动redis文件夹下docker-compose.yml下的密码
     REDIS_DB: 0, //数据库索引
     // 设置数据库的连接信息
     mysqlConfig: {
         // 连接主机
-        host:process.env.NODE_ENV === 'production' ? "127.0.0.1" : "175.178.195.144",
+        host:process.env.NODE_ENV === 'production' ? "127.0.0.1" : "127.0.0.1", // 连接主机
         // 连接端口
-        port: 3306,
-        password: "@Aa292767", // 连接密码 如果开源就是123456
-        user: "jeff",
-        database: "my_store",
+        port: 3306,     // 连接端口
+        password: "123456", // 连接密码 如果开源就是123456 改动这同时需要改动mysql文件夹下docker-compose.yml下的密码
+        user: "my_admin", // 连接用户名     改动这同时需要改动mysql文件夹下docker-compose.yml下的用户名
+        database: "my_store",   // 连接的数据库
     },
     // 需要排除权限验证的接口
     excludePermissionVerification: [
@@ -32,20 +32,18 @@ module.exports = {
         /^\/socket.io/,
 
     ],
-    // 允许跨域的域名
+    // 允许跨域的域名 这里根据需要需要添加本机的公网IP地址 此处没有添加需要自行添加 
     allowCorsOrigins:process.env.NODE_ENV === 'production' ?[
         "http://huchenghe.site",
         "https://huchenghe.site",
         'http://blog.huchenghe.site',
         'https://blog.huchenghe.site',
         'http://www.huchenghe.site',
-        'https://www.huchenghe.site',
-        "http://175.178.195.144",
-        "http://175.178.195.144:5143",
+        'https://www.huchenghe.site', 
+        "http://127.0.0.1:5143",
         "http://127.0.0.1",
     ]: [
         "http://huchenghe.site",
-        "http://175.178.195.144",
         "http://localhost:5143",
         "http://localhost:3000",
         "http://127.0.0.1",
